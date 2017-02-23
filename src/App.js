7eb92fd926e331a70488c18d9decac92beab11fb
George Spake <gpspake@gmail.com>
2017-02-23 14:06:14 -0600

    make store available with react redux provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from 'react'
+import {Provider} from 'react-redux'
+import store from './Redux/store'
 import WidgetList from './Widgets/WidgetList'
 
 const App = React.createClass({
   render() {
     return (
-      <div className="App">
-        <WidgetList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <WidgetList />
+        </div>
+      </Provider>
     );
   }
 })
