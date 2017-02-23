92e368bef2ba5fae6bdfdc6ac72742f97a049dcc
George Spake <gpspake@gmail.com>
2017-02-23 13:19:26 -0600

    create widget list component

---
+++ src/Widgets/WidgetList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WidgetList = React.createClass({
+  render () {
+    return (
+      <div className="widgets">
+        Widgets go here
+      </div>
+    )
+  }
+});
+
+export default WidgetList;
