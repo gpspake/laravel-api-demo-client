a283ecf2cb3555879e08390e75d9bd7a7517a1cb
George Spake <gpspake@gmail.com>
2017-02-27 21:04:49 -0600

    add fetch widget reducers

--- src/Redux/reducers.js
+++ src/Redux/reducers.js
@@ -1,18 +1,49 @@
-import { GET_WIDGETS } from './actions'
+import {
+  FETCH_WIDGETS,
+  FETCH_WIDGETS_SUCCESS,
+  FETCH_WIDGETS_FAILURE,
+  RESET_WIDGETS,
+} from './actions'
 
-const DEFAULT_STATE = {
-  widgets: []
+const INITIAL_STATE = {
+  widgetsList: {
+    widgets: [],
+    error: null,
+    loading: false
+  }
 }
 
-const getWidgets = (state, action) => {
-  const newState = Object.assign({}, state)
-  return newState
+const fetchWidgets = (state) => {
+  const widgetsList = {widgets: [], error: null, loading: true}
+  return Object.assign({}, state, {widgetsList})
 }
 
-const rootReducer = (state = DEFAULT_STATE, action) => {
+const fetchWidgetsSuccess = (state, action) => {
+  const widgetsList = {widgets: action.payload, error: null, loading: false}
+  return Object.assign({}, state, {widgetsList})
+}
+
+const fetchWidgetsFailure = (state, action) => {
+  const error = action.payload || {message: action.payload.message};
+  const widgetsList = {widgets: [], error , loading: false}
+  return Object.assign({}, state, {widgetsList})
+}
+
+const resetWidgets = (state) => {
+  const widgetsList = {widgets: [], error: null, loading: false}
+  return Object.assign({}, state, {widgetsList})
+}
+
+const rootReducer = function (state = INITIAL_STATE, action) {
   switch (action.type) {
-    case GET_WIDGETS:
-      return getWidgets(state, action)
+    case FETCH_WIDGETS:
+      return fetchWidgets(state, action);
+    case FETCH_WIDGETS_SUCCESS:
+      return fetchWidgetsSuccess(state, action)
+    case FETCH_WIDGETS_FAILURE:
+      return fetchWidgetsFailure(state, action)
+    case RESET_WIDGETS:
+      return resetWidgets(state)
     default:
       return state
   }
