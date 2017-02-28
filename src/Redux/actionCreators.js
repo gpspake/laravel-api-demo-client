685cafa11d16854eb75fca64da5cffb9cb4c9718
George Spake <gpspake@gmail.com>
2017-02-27 21:04:25 -0600

    add fetch widget action creators

--- src/Redux/actionCreators.js
+++ src/Redux/actionCreators.js
@@ -1,5 +1,31 @@
-import { GET_WIDGETS } from './actions'
+import axios from 'axios'
 
-export function getWidgets () {
-  return { type: GET_WIDGETS }
+//Widget List
+export const FETCH_WIDGETS = 'FETCH_WIDGETS';
+export const FETCH_WIDGETS_SUCCESS = 'FETCH_WIDGETS_SUCCESS';
+export const FETCH_WIDGETS_FAILURE = 'FETCH_WIDGETS_FAILURE';
+export const RESET_WIDGETS = 'RESET_WIDGETS';
+
+const ROOT_URL = 'http://laravel-api-demo.app/api';
+
+export function fetchWidgets() {
+  const request = axios.get(`${ROOT_URL}/widgets`);
+  return {
+    type: FETCH_WIDGETS,
+    payload: request
+  };
+}
+
+export function fetchWidgetsSuccess(widgets) {
+  return {
+    type: FETCH_WIDGETS_SUCCESS,
+    payload: widgets
+  };
+}
+
+export function fetchWidgetsFailure(error) {
+  return {
+    type: FETCH_WIDGETS_FAILURE,
+    payload: error
+  };
 }
